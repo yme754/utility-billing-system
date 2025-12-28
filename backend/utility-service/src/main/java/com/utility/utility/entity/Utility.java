2b3582928da8d5bf0b71285d05e7d5efac216c54
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-28 22:30:07 +0530

    add utility class

---
+++ backend/utility-service/src/main/java/com/utility/utility/entity/Utility.java
@@ -0,0 +1,22 @@
+package com.utility.utility.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Utility {
+	@Id
+	private String Id;
+	@Indexed(unique = true)
+	private String name;
+	private String unitOfMeasure;
+	private boolean active;
+}
