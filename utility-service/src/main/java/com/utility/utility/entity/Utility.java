17fbdfb665e9804e00d45925289e779cf15827f7
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-28 22:39:55 +0530

    add tariff entity

--- utility-service/src/main/java/com/utility/utility/entity/Utility.java
+++ utility-service/src/main/java/com/utility/utility/entity/Utility.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
