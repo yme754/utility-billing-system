9672ddc518eb9481085541a1a314356855b01eaa
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-28 15:05:53 +0530

    added validation for user entity and updated auth service

--- auth-service/src/main/java/com/utility/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/utility/auth/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.utility.auth.controller;
 
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.utility.auth.dto.AuthRequest;
 import com.utility.auth.dto.AuthResponse;
 import com.utility.auth.service.AuthService;
 import com.utility.auth.util.JwtUtil;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -24,18 +30,30 @@ public class AuthController {
 	private final JwtUtil jwtUtil;
 	
 	@PostMapping("/register")
-	public Mono<ResponseEntity<AuthResponse>> register(@RequestBody AuthRequest request) {
+	public Mono<ResponseEntity<AuthResponse>> register(@Valid @RequestBody AuthRequest request) {
 		return authService.register(request)
 				.map(response-> ResponseEntity.ok(response))
-				.onErrorResume(e-> Mono.just(ResponseEntity.badRequest().body(
-						AuthResponse.builder().message(e.getMessage()).build()
-						)));
+				.onErrorResume(ResponseStatusException.class, e -> Mono.just(
+						ResponseEntity.status(e.getStatusCode())
+						.body(AuthResponse.builder().message(e.getReason()).build())
+						))
+				.onErrorResume(WebExchangeBindException.class, e -> {
+					String errors = e.getBindingResult().getAllErrors().stream()
+							.map(DefaultMessageSourceResolvable::getDefaultMessage)
+							.collect(Collectors.joining(", "));
+					return Mono.just(ResponseEntity.badRequest().body(AuthResponse.builder()
+							.message(errors).build()));
+				})
+				.onErrorResume(Exception.class, e -> Mono.just(
+						ResponseEntity.internalServerError().body(AuthResponse.builder()
+						.message("An unexpected error occured").build()))
+						);
 	}
 	
 	@PostMapping("/login")
 	public Mono<ResponseEntity<AuthResponse>> login(@RequestBody AuthRequest request) {
 		return authService.login(request)
-				.map(response-> ResponseEntity.ok(response))
+				.map(response -> ResponseEntity.ok(response))
 				.onErrorResume(e-> Mono.just(ResponseEntity.status(401).body(
 						AuthResponse.builder().message(e.getMessage()).build()
 						)));
