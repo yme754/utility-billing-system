55ad8c4973a8851dee0744a8e37dd503238592c3
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-28 11:45:24 +0530

    implemented auth response

--- auth-service/src/main/java/com/utility/auth/dto/AuthResponse.java
+++ auth-service/src/main/java/com/utility/auth/dto/AuthResponse.java
@@ -1,4 +1,17 @@
 package com.utility.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+	private String accessToken;
+	private String userId;
+	private String message;
+	private String role;
 }
