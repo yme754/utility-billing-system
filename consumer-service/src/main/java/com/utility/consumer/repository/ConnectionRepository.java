f03c42ffe348faeb0b94241f1b1ce0f3c159a51b
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-28 19:16:27 +0530

    created connection repository

---
+++ consumer-service/src/main/java/com/utility/consumer/repository/ConnectionRepository.java
@@ -0,0 +1,13 @@
+package com.utility.consumer.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.utility.consumer.entity.Connection;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ConnectionRepository extends ReactiveMongoRepository<Connection, String>{
+	Flux<Connection> findAllConsumerId(String consumerId);
+	Mono<Boolean> existsByMeterNumber(String meterNumber);
+}
