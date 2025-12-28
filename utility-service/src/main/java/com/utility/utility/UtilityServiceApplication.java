83438fef7ccd6ddb3ac275e807b12d6c5c685418
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-28 22:23:54 +0530

    create utility service

---
+++ utility-service/src/main/java/com/utility/utility/UtilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.utility.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityServiceApplication.class, args);
+	}
+
+}
