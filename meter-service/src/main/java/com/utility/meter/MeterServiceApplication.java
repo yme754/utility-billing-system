7a5e450e6e95345bca71aaf9d895af4a5a4276b6
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-28 23:21:20 +0530

    create meter service

---
+++ meter-service/src/main/java/com/utility/meter/MeterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.utility.meter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeterServiceApplication.class, args);
+	}
+
+}
