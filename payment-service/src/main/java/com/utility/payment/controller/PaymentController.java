fcf0ce5e52abd31e6ec498fe41f2e45f62f0f799
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-29 21:21:50 +0530

    implement payment controller

---
+++ payment-service/src/main/java/com/utility/payment/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.utility.payment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.utility.payment.entity.Payment;
+import com.utility.payment.service.PaymentService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+	private final PaymentService paymentService;
+	
+	@PostMapping("/pay")
+	public Mono<ResponseEntity<Payment>> makePayment(@RequestBody Payment payment) {
+		return paymentService.processPayment(payment).map(ResponseEntity::ok);
+	}
+}
