4f5ece9bea10029b2904292ebb3881ebeb38896b
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-29 22:30:51 +0530

    update email service and it's service implementation

--- notification-service/src/main/java/com/utility/notification/service/EmailService.java
+++ notification-service/src/main/java/com/utility/notification/service/EmailService.java
@@ -1,33 +1,7 @@
 package com.utility.notification.service;
 
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-
 import com.utility.notification.dto.EmailRequest;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class EmailService {
-	private final JavaMailSender javaMailSender;
-	public void sendEmail(EmailRequest emailRequest) {
-		try {
-			log.info("Sending email to: {}", emailRequest.getTo());
-			MimeMessage message = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
-			helper.setTo(emailRequest.getTo());
-			helper.setSubject(emailRequest.getSubject());
-			helper.setText(emailRequest.getBody(), true);
-			javaMailSender.send(message);
-			log.info("Email sent successfully!");
-		} catch(MessagingException e) {
-			log.error("Failed to send email", e);
-		}
-	}
+public interface EmailService {
+	void sendEmail(EmailRequest emailRequest);
 }
---
+++ notification-service/src/main/java/com/utility/notification/service/EmailServiceImpl.java
@@ -0,0 +1,35 @@
+package com.utility.notification.service;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.utility.notification.dto.EmailRequest;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailServiceImpl implements EmailService{
+	private final JavaMailSender javaMailSender;
+	
+	@Override
+	public void sendEmail(EmailRequest emailRequest) {
+		try {
+			log.info("Sending email to: {}", emailRequest.getTo());
+			MimeMessage message = javaMailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setTo(emailRequest.getTo());
+			helper.setSubject(emailRequest.getSubject());
+			helper.setText(emailRequest.getBody(), true);
+			javaMailSender.send(message);
+			log.info("Email sent successfully!");
+		} catch(MessagingException e) {
+			log.error("Failed to send email", e);
+		}
+	}
+}
