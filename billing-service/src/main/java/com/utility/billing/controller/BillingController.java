5c8d9582b8cad72d601e43d54a1b53fa6aec4c7a
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-29 12:05:33 +0530

    implement get(bill) - get mapping in billing controller

--- billing-service/src/main/java/com/utility/billing/controller/BillingController.java
+++ billing-service/src/main/java/com/utility/billing/controller/BillingController.java
@@ -1,6 +1,8 @@
 package com.utility.billing.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,5 +24,10 @@ public class BillingController {
 	public Mono<ResponseEntity<Bill>> generateBill(@RequestParam String connectionId,
 			@RequestParam String meterId, @RequestParam String utiltyName) {
 		return billingService.generateBill(connectionId, meterId, utiltyName).map(ResponseEntity::ok);
+	}
+	
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<Bill>> getBill(@PathVariable String id) {
+		return billingService.getBill(id).map(ResponseEntity::ok);
 	}
 }
