00858957d4cd1c27ff9e8b087ecfe0a2f85c5129
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-29 10:31:38 +0530

    add bill entity

---
+++ billing-service/src/main/java/com/utility/billing/entity/Bill.java
@@ -0,0 +1,31 @@
+package com.utility.billing.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "bills")
+public class Bill {
+	@Id
+	private String id;
+	private String connectionId;
+	private String meterId;
+	private LocalDate billingDate;
+	private LocalDate dueDate;
+	private Double ratePerUnit;
+	private Double fixedCharge;
+	private Double amount;
+	private Double taxAmount;
+	private Double totalAmount;
+	private String status;
+}
