6cdb426c3c26f2db90022bc5256e0eb3739e3625
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-30 23:32:19 +0530

    updated

---
+++ meter-service/src/main/java/com/utility/meter/config/AuthenticationManager.java
@@ -0,0 +1,36 @@
+package com.utility.meter.config;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import com.utility.meter.util.JwtUtil;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationManager implements ReactiveAuthenticationManager{
+	private final JwtUtil jwtUtil;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        String authToken = authentication.getCredentials().toString();
+        if (!jwtUtil.validateToken(authToken)) return Mono.empty();
+        Claims claims = jwtUtil.getAllClaimsFromToken(authToken);
+        String username = claims.getSubject();
+        List<String> roles = claims.get("roles", List.class);
+        List<SimpleGrantedAuthority> authorities = roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return Mono.just(new UsernamePasswordAuthenticationToken(username, authToken, authorities));
+    }
+}
