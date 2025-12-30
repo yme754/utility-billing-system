c76267e054f49a1eb84ff463c5619a54750e7290
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-30 23:44:14 +0530

    updated

---
+++ billing-service/src/main/java/com/utility/billing/util/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.utility.billing.util;
+
+import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+	@Value("${jwt.secret}")
+    private String secret;
+
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) { return false; }
+    }
+
+    public String extractUsername(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
