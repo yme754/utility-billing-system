cc433e4dd20c941cdc97a78a94770a4bec4eae0b
Yaswitha Sakhamuri <yaswitha@example.com>
2026-01-02 14:14:06 +0530

    fix CORS issue

--- auth-service/src/main/java/com/utility/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/utility/auth/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.utility.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,27 +18,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
-	private final AuthenticationManager authenticationManager;
-    private final SecurityContextRepository securityContextRepository;
-	
+	private final ReactiveAuthenticationManager authenticationManager;
+    private final ServerSecurityContextRepository securityContextRepository;
+
     @Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-    	return http
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)                
+                .cors(ServerHttpSecurity.CorsSpec::disable)
                 .authenticationManager(authenticationManager)
                 .securityContextRepository(securityContextRepository)
                 .authorizeExchange(exchanges -> exchanges
-                    .pathMatchers("/auth/login", "/auth/register", "/auth/validate").permitAll()
-                    .pathMatchers("/actuator/**").permitAll()
-                    .anyExchange().authenticated()
+                        .pathMatchers("/auth/login", "/auth/register", "/auth/validate").permitAll()
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated()
                 )
                 .build();
-        }
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
