3317befecb2ad918205a41b8d94cb26df5e6c7ea
Yaswitha Sakhamuri <yaswitha@example.com>
2026-01-04 04:24:52 +0530

    update Dockerfile, notification body, properties

--- notification-service/src/main/java/com/utility/notification/service/EmailServiceImpl.java
+++ notification-service/src/main/java/com/utility/notification/service/EmailServiceImpl.java
@@ -2,10 +2,12 @@ package com.utility.notification.service;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.utility.notification.dto.EmailRequest;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,13 +21,25 @@ public class EmailServiceImpl implements EmailService{
 	public void sendEmail(EmailRequest request) {
         log.info("Sending email to: {}", request.getTo());
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("yxsh2999@gmail.com");
-            message.setTo(request.getTo());
-            message.setSubject(request.getSubject());
-            message.setText(request.getBody());
+        	MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom("yxsh2999@gmail.com");
+            helper.setTo(request.getTo());
+            helper.setSubject(request.getSubject());
+            String htmlContent = String.format(
+                "<div style='font-family:sans-serif; padding:20px; border:1px solid #eee; border-radius:10px;'>" +
+                "<h2 style='color:#2563EB;'>Utilix Notification</h2>" +
+                "<p>%s</p>" +
+                "<hr style='border:none; border-top:1px solid #eee; margin:20px 0;'>" +
+                "<p style='font-size:12px; color:#666;'>This is an automated message from the Utilix Billing System.</p>" +
+                "</div>", 
+                request.getBody().replace("\n", "<br>")
+            );
+
+            helper.setText(htmlContent, true);
             javaMailSender.send(message);
-            log.info("Email sent successfully!");
+            log.info("HTML Email sent successfully!");
         } catch (Exception e) {
             log.error("Failed to send email: {}", e.getMessage());
         }
